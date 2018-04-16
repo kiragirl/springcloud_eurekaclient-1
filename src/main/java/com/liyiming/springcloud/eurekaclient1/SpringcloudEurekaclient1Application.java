09d68b155ffca7abb484fe73c085906d7537fd13
liyiming <liyiming0215@sina.com>
2018-04-16 09:19:38 +0800

    origin

---
+++ src/main/java/com/liyiming/springcloud/eurekaclient1/SpringcloudEurekaclient1Application.java
@@ -0,0 +1,14 @@
+package com.liyiming.springcloud.eurekaclient1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringcloudEurekaclient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaclient1Application.class, args);
+	}
+}
