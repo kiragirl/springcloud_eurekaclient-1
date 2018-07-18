201442114236b923dce60bc151f401ddcaba5d00
liyiming <liyiming0215@sina.com>
2018-07-18 16:32:48 +0800

    服务链路追踪(Spring Cloud Sleuth)

--- src/main/java/com/liyiming/springcloud/eurekaclient1/controller/HelloController.java
+++ src/main/java/com/liyiming/springcloud/eurekaclient1/controller/HelloController.java
@@ -6,6 +6,8 @@
  */
 package com.liyiming.springcloud.eurekaclient1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,11 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
     @Value("${server.port}")
     String port;
     @RequestMapping("/hi")
     public String home(@RequestParam String name) {
+    	log.info("eureka client 1 being calling ");
         return "hi "+name+",i am from port:" +port;
     }
 }
